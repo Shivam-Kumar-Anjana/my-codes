f4fc5b93be9574854d885f4b77217e95d787bc0c
Shivam-Kumar-Anjana <80809227+Shivam-Kumar-Anjana@users.noreply.github.com>
2021-03-23 10:19:32 +0530

    Create credit card luhn algorithm.c

---
+++ credit card luhn algorithm.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<cs50.h>
+
+int main()
+{
+    long n=0, card[n], i=0;
+//luhn algorith
+
+   //printf("enter the card number");
+
+    for(i=0;i<=n;i++)
+   {
+    card[i]= get_long("card number:");
+    //printf("%ld", card[i]);
+   }
+   //for( i=0;i<=n;i++)
+   //{
+      int k = card[i];
+      printf("%d",k);
+   //}
+
+
+}
