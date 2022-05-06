8af6391f41224b20152515c9845414d786c90915
Shivam-Kumar-Anjana <80809227+Shivam-Kumar-Anjana@users.noreply.github.com>
2022-05-06 17:48:24 +0530

    Create Odd_even.py

---
+++ Odd_even.py
@@ -0,0 +1,8 @@
+num=input("Enter your number : ")
+if num==0:
+    print("Number is zero ")
+elif num%2:
+    print("Number is even")
+else:
+    print("Number is odd")
+print("This will give error")
